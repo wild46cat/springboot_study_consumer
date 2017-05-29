ab09f6ec41e414b8580f28cfb3a0c085aecc0fa7
WuXueyou <409898859@qq.com>
2017-05-29 19:17:40 +0800

    first commit

---
+++ src/main/java/com/xueyou/demo/App.java
@@ -0,0 +1,35 @@
+package com.xueyou.demo;
+
+import dto.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ */
+
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    public static void main(String[] args) {
+        //get方式
+//        Student student = restTemplate.getForObject("http://localhost:8080/test/greeting2?id=10&name=xueyou", Student.class);
+//        logger.info(student.toString());
+        //post方式
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("id", "10");
+        requestParams.add("name", "xueyou");
+        Student student2 = restTemplate.postForObject("http://localhost:8080/test/greeting2", requestParams, Student.class);
+        logger.info(student2.toString());
+
+    }
+}
---
+++ src/main/java/dto/Student.java
@@ -0,0 +1,40 @@
+package dto;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * Created by wuxueyou on 2017/5/29.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}
